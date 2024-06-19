a3cd7debb1574012a0aa943bc899407701b82296
ayoubamqicher <ayoubaqmicher@gmail.com>
2024-06-19 17:53:55 +0000

    adding user and Role models

---
+++ src/main/java/com/example/jibibackend/model/Role.java
@@ -0,0 +1,20 @@
+package com.example.jibibackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/java/com/example/jibibackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.jibibackend.repository;
+
+import com.example.jibibackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
